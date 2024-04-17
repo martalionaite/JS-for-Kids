ff6bbdc78c1cff8543b09b821fabf735704bda76
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-04-17 15:43:15 +0300

    string and number

---
+++ kids.js
@@ -0,0 +1,74 @@
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAnHour);
+
+var hoursInADay = 24;
+var secondsInaDay = secondsInAnHour * hoursInADay;
+console.log(secondsInaDay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInaDay * daysInAYear;
+console.log(secondsInAYear);
+
+var age = 30;
+var sum = age * secondsInAYear;
+console.log(sum);
+
+var highFives = 0;
+++highFives;
+++highFives;
+--highFives;
+console.log(highFives);
+
+var x = 10;
+x = x + 5;
+console.log(x);
+
+var score = 10;
+score += 7;
+console.log(score);
+
+var score1 = 8;
+score1 -= 3;
+console.log(score1);
+
+var balloons = 100;
+balloons *= 2;
+console.log(balloons);
+
+var balloons = 100;
+balloons /= 4;
+console.log(balloons);
+
+var myAwesomeString = "Something RELLY awesome!!!";
+console.log(myAwesomeString);
+
+var myThing = 5;
+myThing = "This is a string";
+console.log(myThing);
+
+var numberNine = 9;
+var stringNine = "9";
+
+sum = numberNine + numberNine;
+sum1 = stringNine + stringNine;
+console.log(sum, sum1);
+
+var greeting = "Hello ";
+var myName = "Nick";
+result = greeting + myName;
+console.log(result);
+
+var text = "Supercalifragilisticexpialidocious".length;
+console.log(text);
+
+var text1 = "Java".length;
+console.log(text1);
+
+var script = "Script".length;
+console.log(script);
+
+var javascript = "java" + "script";
+console.log(javascript.length);
+
